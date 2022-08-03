23b35c6225a4a3873eced93229ecb2a410f9d125
gotomarina <marina.goto@rakus-partners.co.jp>
2022-08-03 14:59:25 +0900

    2

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +17,9 @@ import jp.co.sample.service.AdministratorService;
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
+	
+	@Autowired
+	private HttpSession session;
 	
 	@Autowired
 	private AdministratorService administratorService;
@@ -48,6 +54,18 @@ public class AdministratorController {
 	public String toLogin() {
 		return "administrator/login";
 		//ログイン画面へ
+	}
+	
+	@RequestMapping("/login")
+	public String login(LoginForm form,Model model) {
+		Administrator administrator=administratorService.login(form.getMailAddress(), form.getPassword());
+		
+		if(administrator==null) {
+			model.addAttribute("errormessage", "メールアドレスまたはパスワードが不正です。");
+		}else {
+			model.addAttribute("administratorName", administrator);
+		}
+		return "forward:/employee/showList";
 	}
 
 }
